cbeadda76eaa857aca7e7eef30e4efb9403c2903
Ofek <ofek@outlook.com>
2014-04-20 02:42:08 +0300

    add mapsengine map

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-      function initialize() {
-          var mapOptions = {
-              center: new google.maps.LatLng(-34.397, 150.644),
-              zoom: 8
-          };
-          var map = new google.maps.Map(document.getElementById("map-canvas"),
-              mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-
       $(document).ready(function (e) {
           $('.with-hover-text, .regular-link').click(function (e) {
               e.stopPropagation();
